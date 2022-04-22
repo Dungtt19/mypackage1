859eb7b50e97d0f05d2fe73bd7367075eb8d43a0
Dungtt19 <19211tt0992@mail.tdc.edu.vn>
2022-04-22 16:45:11 +0700

    commit source mypackage1

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '65756029cdaf4dc0c50a148a5a328e83ec4c9160',
+        'name' => 'dungtt19/mypackage1',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'dungtt19/mypackage1' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '65756029cdaf4dc0c50a148a5a328e83ec4c9160',
+            'dev_requirement' => false,
+        ),
+    ),
+);
